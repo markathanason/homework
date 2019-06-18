02a5362e5a76c282806a040f687238aca45d55b7
markathanason <athanason0120@gmail.com>
2019-06-18 00:25:32 -0500

    added python homework

---
+++ PyBank/main.py
@@ -0,0 +1,53 @@
+import csv
+import os
+
+budget_data_path = "Resources/budget_data.csv"
+
+date = []
+profit_losses = []
+
+with open(budget_data_path, newline="", encoding="utf8") as budget_data:
+	csvreader = csv.reader(budget_data, delimiter=",")
+	next(csvreader)
+	for row in csvreader:
+
+		date.append(str(row[0]))
+
+		profit_losses.append(int(row[1]))
+		#iterates through and adds the 2nd value to the previous
+		i = [profit_losses[i+1] - profit_losses[i] for i in range(len(profit_losses)-1)]
+
+zipped = zip(date[1::], i[::])
+zipped_list = list(zipped)
+
+profit_losses_sum = sum(profit_losses)
+profit_losses_average = sum(i) / len(i)
+
+increase = max(i)
+decrease = min(i)
+
+month_dec = 0
+month_inc = 0
+
+for row in zipped_list:
+	if row[1] == increase:
+		month_inc = row[0]
+	if row[1] == decrease:
+		month_dec = row[0]
+
+with open('Resources/output.txt', 'w') as text_file:
+	print(f'Financial Analysis', file=text_file)
+	print(f'___________________________', file=text_file)
+	print(f'Total Months: {len(date)}', file=text_file)
+	print(f'Total: ${profit_losses_sum}', file=text_file)
+	print(f'Average Change: ${profit_losses_average:.2f}', file=text_file)
+	print(f'Greatest Increase in Profits: {month_inc} ({increase})', file=text_file)
+	print(f'Greatest Decrease in Profits: {month_dec} ({decrease})', file=text_file)
+
+print("Financial Analysis")
+print("-----------------------------------")
+print(f'Total Months: {len(date)}')
+print(f'Total: ${profit_losses_sum}')
+print(f'Average Change: ${profit_losses_average:.2f}')
+print(f'Greatest Increase in Profits: {month_inc} ({increase})')
+print(f'Greatest Decrease in Profits: {month_dec} ({decrease})')
---
+++ PyPoll/main.py
@@ -0,0 +1,49 @@
+import csv
+
+election_data_path = "Resources/election_data.csv"
+
+manifest = []
+individual = []
+vote_count = []
+vote_percent =[]
+count = 0
+
+with open(election_data_path, newline="", encoding="utf8") as election_data:
+	csvreader = csv.reader(election_data, delimiter=",")
+	next(csvreader)
+	for row in csvreader:
+		count = count+1
+		manifest.append(row[2])
+	for i in set(manifest):
+		individual.append(i)
+		j = manifest.count(i)
+		vote_count.append(j)
+		k = (j/count)*100
+		vote_percent.append(k)
+
+	winner_count = max(vote_count)
+	w = individual[vote_count.index(winner_count)]
+
+with open('Resources/output.txt', 'w') as output:
+	print("-----------------------", file=output)
+	print("Election Results", file=output)   
+	print("-----------------------", file=output)
+	print(f'Total Votes : {str(count)}', file=output)    
+	print("-----------------------", file=output)
+	for i in range(len(individual)):
+		print(f'{individual[i]} : %{vote_percent[i]:.3f} {vote_count[i]}', file=output)
+	print("-----------------------", file=output)
+	print(f'The winner is: {w}', file=output)
+	print("-----------------------", file=output)
+
+
+print("-----------------------")
+print("Election Results")   
+print("-----------------------")
+print(f'Total Votes : {str(count)}')    
+print("-----------------------")
+for i in range(len(individual)):
+	print(f'{individual[i]} : %{vote_percent[i]:.3f} {vote_count[i]}')
+print("-----------------------")
+print(f'The winner is: {w}')
+print("-----------------------")
